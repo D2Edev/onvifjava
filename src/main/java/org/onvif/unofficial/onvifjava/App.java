e8ec49ecc0ffe4d31b6829428926477b1e385fb2
Dmitry Druzhynin <dmitry.druzhynin@gmail.com>
2017-11-02 07:41:30 +0200

    first commit

---
+++ src/main/java/org/onvif/unofficial/onvifjava/App.java
@@ -0,0 +1,13 @@
+package org.onvif.unofficial.onvifjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
