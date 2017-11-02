3b0939ae16bd97cef4931bb428fb7419b3db1756
Dmitry Druzhynin <dmitry.druzhynin@gmail.com>
2017-11-02 14:15:34 +0200

    mass update

---
+++ src/main/java/org/oasis/open/docs/wsrf/r_2/ResourceUnavailableFaultType.java
@@ -0,0 +1,35 @@
+
+package org.oasis.open.docs.wsrf.r_2;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+import org.oasis.open.docs.wsrf.bf_2.BaseFaultType;
+
+
+/**
+ * <p>Java class for ResourceUnavailableFaultType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ResourceUnavailableFaultType">
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://docs.oasis-open.org/wsrf/bf-2}BaseFaultType">
+ *       &lt;anyAttribute processContents='lax' namespace='##other'/>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ResourceUnavailableFaultType")
+public class ResourceUnavailableFaultType
+    extends BaseFaultType
+{
+
+
+}
